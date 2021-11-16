87f80ecda0882f1f6ea84ada01378ef61bd4336e
zulhaq555 <zulhaq555@gmail.com>
2021-11-16 17:38:20 +0000

    Added game Sprites and tilesets/Maps

--- src/TileMap/TileMap.java
+++ src/TileMap/TileMap.java
@@ -1,5 +1,8 @@
 package TileMap;
 
+import com.main.GamePanel;
+
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 
 public class TileMap {
@@ -19,7 +22,42 @@ public class TileMap {
 
     //tileset
     private BufferedImage tileSet;
-    private int numTilesAccross;
+    private int numTilesAcross, numTileHigh;
     private Tile[][] tiles;
+
+    //rendering
+    private int rowOffset, colOffset, numRowsToDraw, numColsToDraw;
+
+    public TileMap(int tileSize){
+        this.tileSize = tileSize;
+        numRowsToDraw = GamePanel.HEIGHT / tileSize + 2;
+        numColsToDraw = GamePanel.WIDTH / tileSize + 2;
+        smoothScroll = 0.07;
+    }
+
+    public void loadTiles(String s){
+        try{
+            tileSet = ImageIO.read(getClass().getResourceAsStream(s));
+
+            numTilesAcross = tileSet.getWidth() / tileSize;
+            numTileHigh = tileSet.getHeight() / tileSize;
+
+            tiles = new Tile[numTileHigh][numTilesAcross];
+
+            BufferedImage subImage;
+            for (int col = 0; col < numTilesAcross; col++) {
+                for (int row = 0; row < numTileHigh; row++) {
+
+                    subImage = tileSet.getSubimage(col * tileSize, row * tileSize, tileSize, tileSize);
+                    tiles[row][col] = new Tile(subImage, Tile.BLOCKED);
+
+                }
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 
 }
